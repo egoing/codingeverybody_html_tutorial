638d4e41a49e35c970c63cc189cd62b1b33bc2be
egoing <egoing@gmail.com>
2012-10-01 15:40:42 +0900

    폼 예제 추가

---
+++ form_14/example2.php
@@ -0,0 +1,10 @@
+<html>
+        <head>
+                <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+        </head>
+        <body>
+                <?php
+                echo $_REQUEST['nickname'].'님의 직업은 '.$_REQUEST['job'].'이군요!';
+                ?>
+        </body>
+</html>
